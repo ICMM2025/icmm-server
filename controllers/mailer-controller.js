94f7c9b77c78d436e28785117eb8dc6b9d92b1b3
BesRoom <info.besroom@gmail.com>
2025-09-22 10:46:56 +0700

    fix email

--- controllers/mailer-controller.js
+++ controllers/mailer-controller.js
@@ -2,17 +2,28 @@ const prisma = require("../models/index");
 const tryCatch = require("../utils/try-catch");
 const createError = require("../utils/create-error");
 const transporter = require("../utils/mailer");
+const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
+const mailerSend = new MailerSend({
+  apiKey: process.env.MAILERSEND_API_KEY,
+});
 
 module.exports.sendMail = tryCatch(async (req, res, next) => {
   const { to, subject, text } = req.body;
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text,
-  };
 
-  await transporter.sendMail(mailOptions);
+  const sentFrom = new Sender("info@icmm.run", "Intania Runner Club");
+  const recipients = [new Recipient(to, "Customer")];
+
+  const htmlText = text.replace(/\n/g, "<br>");
+  const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipients)
+    .setReplyTo(sentFrom)
+    .setSubject(subject)
+    .setHtml(htmlText)
+    .setText(text);
+
+  await mailerSend.email.send(emailParams);
+
   res.json({
     message: "Send email successful...",
   });
